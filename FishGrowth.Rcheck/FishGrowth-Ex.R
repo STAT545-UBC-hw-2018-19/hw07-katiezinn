2a6618ccc0a15395e1314f72b4c60cd81b22a55f
Katie Zinn <katiezinn@gmail.com>
2018-11-15 20:26:18 -0800

    package update

--- FishGrowth.Rcheck/FishGrowth-Ex.R
+++ FishGrowth.Rcheck/FishGrowth-Ex.R
@@ -23,6 +23,8 @@ nameEx("hello")
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+
 
 base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
 base::cat("hello", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
