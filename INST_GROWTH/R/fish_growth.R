86143693222e71f4e56e904f1aecd1e6fabeba30
Katie Zinn <katiezinn@gmail.com>
2018-11-15 20:29:13 -0800

    removing first package attempt

--- INST_GROWTH/R/fish_growth.R
+++
@@ -1,6 +0,0 @@
-#' Fish data
-#' Including initial length, final length, and days between measurements
-#' Self-explanatory!
-#' @format This (and the next) is a new tag that applies for data documentation.
-#' @source Note that you shouldn't use the `@export` tag when documenting data!
-"fish_data"
